2ed40d4ebf6f9a3d89a85d7d68dc2d30c7638966
Anna.VINAR <anna.vinar@1000geeks.com>
2021-07-13 16:48:52 +0300

    Add fix isOwner conditions, add Install MetaMask info

--- ethereum-client/client/src/pages/Connect/Connect.js
+++ ethereum-client/client/src/pages/Connect/Connect.js
@@ -3,16 +3,25 @@ import React from "react";
 import "./Connect.scss";
 
 const Connect = (props) => {
+  console.log(props.children);
+  const info = (
+      <>
+        <button type="button" className="btn-primary" onClick={props.connect}>
+          {" "}
+          Connect Metamask{" "}
+        </button>
+        <p> please connect first </p>
+      </>
+  )
   return (
     <div className="connect">
       <div className="container">
         <img src="/images/blockchain.svg" alt="Blockchain" />
         <div className="connect__info">
-          <button type="button" className="btn-primary" onClick={props.connect}>
-            {" "}
-            Connect Metamask{" "}
-          </button>
-          <p> please connect first </p>
+          {props.children
+              ? props.children
+              : info
+          }
         </div>
         <img src="/images/prive.svg" alt="prive" />
       </div>
