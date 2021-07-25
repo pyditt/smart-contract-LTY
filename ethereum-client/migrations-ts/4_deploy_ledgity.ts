2843ba05bde3190e9999ee76b9b5c61d5f21a8c6
Oleh Misarosh <oleh.misarosh@1000geeks.com>
2021-07-25 23:48:12 +0300

    Add Ledgity deployment

--- ethereum-client/migrations-ts/4_deploy_ledgity.ts
+++
@@ -1,9 +0,0 @@
-const Ledgity = artifacts.require('Ledgity');
-
-const migration: Truffle.Migration = async function (deployer) {
-  await deployer.deploy(Ledgity);
-};
-module.exports = migration;
-
-// because of https://stackoverflow.com/questions/40900791/cannot-redeclare-block-scoped-variable-in-unrelated-files
-export { };
