e4c5b0ab280182f0e0b8627295cb9d5d5d0fa5d3
Oleh Misarosh <oleh.misarosh@1000geeks.com>
2021-08-05 14:39:12 +0300

    Swap exactly numTokensToSwap

--- ethereum-client/contracts/Ledgity.sol
+++ ethereum-client/contracts/Ledgity.sol
@@ -177,6 +177,9 @@ contract Ledgity is ILedgity, ReflectToken {
             !inSwapAndLiquify &&
             sender != address(uniswapV2Pair)
         ) {
+            if (contractTokenBalance > numTokensToSwap) {
+                contractTokenBalance = numTokensToSwap;
+            }
             _swapAndLiquifyOrCollect(contractTokenBalance);
         }
     }
