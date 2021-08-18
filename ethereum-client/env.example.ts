ed3fdcac47fba4e37d3ce91799fc3ba8f3569df5
Roman.YARINSKIY <roman.yarinskiy@1000geeks.com>
2021-08-18 14:48:24 +0000

    Add: config to deploy to main net

--- ethereum-client/env.example.ts
+++ ethereum-client/env.example.ts
@@ -4,7 +4,10 @@
 export const PRIVATE_NETWORK_PRIVATE_KEY = '';
 
 export const ROPSTEN_PRIVATE_KEY = '';
+
+export const MAINNET_PRIVATE_KEY = '';
 /**
  * E.g., Infura ropsten url.
  */
 export const ROPSTEN_PROVIDER_URL = '';
+export const MAINNET_PROVIDER_URL ='';
